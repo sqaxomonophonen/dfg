64d094216b49fa050220092c2ce9bf234c411ad1
Anders Kaare Straadt <anders@straadt.dk>
2023-11-30 15:26:10 +0100

    more interesting "song"

--- song0/song.c
+++ song0/song.c
@@ -6,7 +6,9 @@ BUS song(void)
 	const int n_beats = 4*4;
 	CURVE gain_curve = NULL;
 	for (int beat = 0; beat < n_beats; beat++) {
-		curve_add1(&gain_curve, 0, 1.0, -1.0);
+		curve_add1(&gain_curve, 0,
+			(beat&3) == 0 ? 1.0 : 0.5,
+			(beat&3) == 0 ? -1.0 : -0.5);
 		adv(1);
 	}
 
@@ -16,8 +18,10 @@ BUS song(void)
 	BUS half_height = constant(0);
 	BUS zero_wait = constant(0);
 	BUS hw = hexwave(freq,reflect,peak_time,half_height,zero_wait);
-	BUS a = mul(hw,curvegen(gain_curve));
-	a = mul(a,constant(0.04));
+	BUS cu = curvegen(gain_curve);
+	cu = mul(cu,cu);
+	BUS a = mul(hw,cu);
+	a = mul(a,constant(0.2));
 
 	return a;
 }
